c6a36a31b8e3ef75bdfddc90b10cde6c79cdb57e
Janki Chauhan <janki.chauhan@gmail.com>
2020-05-10 23:22:33 -0700

    first commit, adding folders

---
+++ src/GetData.py
@@ -0,0 +1,27 @@
+import traceback
+import time
+import urllib3
+import xmltodict
+from random import seed
+from random import randint
+seed(1)
+
+class GetData:
+    def get_data_by_id(id):
+        url = "https://www.boardgamegeek.com/xmlapi/boardgame/"+id+"?stats=1"
+        http = urllib3.PoolManager()
+
+        response = http.request('GET', url)
+        try:
+            data = xmltodict.parse(response.data)
+            with open('data/game'+id+'.xml', 'wb') as file:
+                file.write(response.data)
+        except:
+            print("Failed to parse xml from response (%s)" % traceback.format_exc())
+        return data
+
+
+for i in range(1000):
+    value = randint(0, 10000)
+    GetData.get_data_by_id(str(value))
+    time.sleep(4)
