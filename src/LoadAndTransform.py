f201e8a492d72d267374c5e8b3d3b279fc6e1d9a
Janki Chauhan <janki.chauhan@gmail.com>
2020-05-13 16:31:18 -0700

    New EDAs and updated Modelling

--- src/LoadAndTransform.py
+++ src/LoadAndTransform.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from src.Modelling import Model
+from collections import Counter
+from itertools import chain
 
 class LoadAndTransform():
     def __init__(self):
@@ -11,28 +13,37 @@ class LoadAndTransform():
                                 'designer', 'category', 'mechanic', 'publisher', 'age', 'rank']
         self.board_game_data = self.df_data
 
+        self.unique_publishers = []
+        self.unique_categories = []
+        self.unique_designers = []
+
     def tranform(self):
         self.df_data = pd.read_csv('board_game_data.csv')
         self.df_data.columns = ['board_game_id', 'name', 'year', 'minplayer', 'maxplayer', 'playingtime', 'avgratings',
                                 'designer', 'category', 'mechanic', 'publisher', 'age', 'rank']
         self.board_game_data = self.df_data
-        self.board_game_data = self.board_game_data.fillna('not define')
+        self.board_game_data = self.board_game_data.fillna("not define")
         self.board_game_data['designer'] = self.board_game_data['designer'].str.replace(':', ', ')
         self.board_game_data['category'] = self.board_game_data['category'].str.replace(':', ', ')
         self.board_game_data['mechanic'] = self.board_game_data['mechanic'].str.replace(':', ', ')
         self.board_game_data['publisher'] = self.board_game_data['publisher'].str.replace(':', ', ')
 
+        self.get_categorical_list()
+        self.one_hot_encoding()
+        self.data_for_modelling()
+
+
     def data_for_modelling(self):
         columns = self.board_game_data.columns.tolist()
         columns = [c for c in columns if
-                   c not in ['board_game_id', 'name', 'year', 'rank', 'designer', 'category', 'mechanic', 'publisher']]
+                   c not in ['board_game_id','avgratings', 'name', 'year', 'rank', 'designer', 'category', 'mechanic', 'publisher', 'cat_not define','pub_not define','des_not define', 'cat_(Uncredited)']]
         target = 'avgratings'
 
         X = self.board_game_data[columns].values
         y = self.board_game_data[target].values
 
-        print(X)
-        print(y)
+        print(columns)
+        print(self.board_game_data.shape)
 
         # for col in columns:
         #     plt.scatter(self.board_game_data[col], self.board_game_data[target], alpha=0.4)
@@ -41,10 +52,62 @@ class LoadAndTransform():
         #     plt.show()
 
         ml_model = Model(X,y)
-        ml_model.LinearReg()
-        ml_model.RandomForest()
-        ml_model.GradientBoost()
+        # ml_model.LinearReg()
+        ml_model.RandomForest(columns)
+        # ml_model.GradientBoost()
+        # ml_model.grid_search_cv_gb()
+        # ml_model.grid_search_cv_rf()
+
+    def get_categorical_list(self):
+        board_game_data_copy = self.board_game_data.copy()
+        board_game_data_copy = board_game_data_copy[board_game_data_copy['rank'] != 0]
+        board_game_data_copy = board_game_data_copy[board_game_data_copy['designer'] != '(Uncredited)']
+        board_game_data_copy = board_game_data_copy[board_game_data_copy['designer'] != 'not define']
+        board_game_data_copy = board_game_data_copy[board_game_data_copy['designer'] != ' ']
+        board_game_data_copy = board_game_data_copy[board_game_data_copy['publisher'] != '(Self-Published)']
+        board_game_data_copy = board_game_data_copy[board_game_data_copy['publisher'] != '(Web published)']
+        board_game_data_copy = board_game_data_copy.sort_values("rank")
+
+        board_game_data_copy['mechanic'] = board_game_data_copy['mechanic'].str.rsplit(',').str[0]
+        board_game_data_copy['designer'] = board_game_data_copy['designer'].str.rsplit(',').str[0]
+        board_game_data_copy['category'] = board_game_data_copy['category'].str.rsplit(',').str[0]
+        board_game_data_copy['publisher'] = board_game_data_copy['publisher'].str.rsplit(',').str[0]
+
+        board_game_data_copy = board_game_data_copy[:1000]
+
+        category_count = board_game_data_copy.groupby(['category']).count()[['name']]
+        self.unique_categories = list(category_count.sort_values('name', ascending=False).head(10).index)
+
+        desinger_count = board_game_data_copy.groupby(['designer']).count()[['name']]
+        self.unique_designers = list(desinger_count.sort_values('name', ascending=False).head(10).index)
+
+        publisher_count = board_game_data_copy.groupby(['publisher']).count()[['name']]
+        publisher_count = publisher_count[publisher_count.index != '(Web published)']
+        publisher_count = publisher_count[publisher_count.index != '(Self-Published)']
+        self.unique_publishers = list(publisher_count.sort_values('name', ascending=False).head(10).index)
+
+    def category_list(self):
+        unique_categories = self.board_game_data['category'].tolist()
+        unique_categories = [category.split(', ') for category in unique_categories]
+        unique_categories = set(chain(*unique_categories))
+        return unique_categories
+
+    def designer_list(self):
+        unique_designers = self.board_game_data['designer'].tolist()
+        unique_designers = [designer.split(', ') for designer in unique_designers]
+        unique_designers = set(chain(*unique_designers))
+        return unique_designers
+
+    def one_hot_encoding(self):
+        for category in self.unique_categories:
+            self.board_game_data["cat_"+category] = self.board_game_data.category.apply(lambda x: 1 if category in x else 0)
+        for publisher in self.unique_publishers:
+            self.board_game_data["pub_"+publisher] = self.board_game_data.publisher.apply(lambda x: 1 if publisher in x else 0)
+        for designer in self.unique_designers:
+            self.board_game_data["des_"+designer] = self.board_game_data.designer.apply(lambda x: 1 if designer in x else 0)
+
+
 
 
 LoadAndTransform().tranform()
-LoadAndTransform().data_for_modelling()
+# LoadAndTransform().data_for_modelling()
--- src/Modelling.py
+++ src/Modelling.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.model_selection import train_test_split,KFold,cross_val_score,GridSearchCV
+import matplotlib.pyplot as plt
 
 
 class Model:
@@ -20,15 +21,76 @@ class Model:
         linear_model = LinearRegression().fit(self.X_train, self.y_train)
         predictions = linear_model.predict(self.X_test)
         print(mean_squared_error(self.y_test, predictions))
+        print(r2_score(self.y_test, predictions))
+        print(explained_variance_score(self.y_test, predictions))
 
 
-    def RandomForest(self):
-        random_forest = RandomForestRegressor().fit(self.X_train, self.y_train)
+
+    def RandomForest(self, features):
+        random_forest = RandomForestRegressor(bootstrap=True, ccp_alpha=0.0, criterion='mse',
+                      max_depth=90, max_features=5, max_leaf_nodes=None,
+                      max_samples=None, min_impurity_decrease=0.0,
+                      min_impurity_split=None, min_samples_leaf=3,
+                      min_samples_split=12, min_weight_fraction_leaf=0.0,
+                      n_estimators=1000, n_jobs=None, oob_score=False,
+                      random_state=None, verbose=0, warm_start=False).fit(self.X_train, self.y_train)
         predictions = random_forest.predict(self.X_test)
         print(mean_squared_error(self.y_test, predictions))
+        print(r2_score(self.y_test, predictions))
+        print(explained_variance_score(self.y_test, predictions))
+        print(random_forest.feature_importances_)
+        print(predictions)
+        print(self.y_test)
+
+        feat_scores = pd.DataFrame({'Fraction of Samples Affected': random_forest.feature_importances_},
+                                   index=features)
+        feat_scores = feat_scores.sort_values(by='Fraction of Samples Affected')
+        feat_scores.plot(kind='barh')
+        plt.show()
+
 
 
     def GradientBoost(self):
-        gradient_boost = GradientBoostingRegressor().fit(self.X_train, self.y_train)
+        gradient_boost = GradientBoostingRegressor(learning_rate=0.2, n_estimators=1000, max_features=20 ).fit(self.X_train, self.y_train)
         predictions = gradient_boost.predict(self.X_test)
         print(mean_squared_error(self.y_test, predictions))
+        print(r2_score(self.y_test, predictions))
+        print(explained_variance_score(self.y_test, predictions))
+        print(predictions)
+        print(self.y_test)
+
+    def grid_search_cv_rf(self):
+        param_grid = {
+            'bootstrap': [True],
+            'max_depth': [80, 90, 100, 110],
+            'max_features': [2, 3],
+            'min_samples_leaf': [3, 4, 5],
+            'min_samples_split': [8, 10, 12],
+            'n_estimators': [1000, 2000, 3000]
+        }
+        rf_gridsearch = GridSearchCV(RandomForestRegressor(),
+                                     param_grid,
+                                     n_jobs=-1,
+                                     verbose=True)
+        rf_gridsearch.fit(self.X_train, self.y_train)
+
+        print("best parameters:", rf_gridsearch.best_params_)
+
+        print(rf_gridsearch.best_estimator_)
+
+    def grid_search_cv_gb(self):
+        gb_grid = {'n_estimators': [1000, 2000, 3000],
+                   'min_samples_leaf': [1, 2, 3],
+                   'max_depth': [5, 10, 15],
+                   'max_features': [10, 15, 20],
+                   'learning_rate': [.05, .1, .15, .2]}
+        gb_gridsearch = GridSearchCV(GradientBoostingRegressor(),
+                                     gb_grid,
+                                     n_jobs=-1,
+                                     verbose=True,
+                                     scoring='accuracy')
+        gb_gridsearch.fit(self.X_train, self.y_train)
+
+        print("best parameters:", gb_gridsearch.best_params_)
+
+        print(gb_gridsearch.best_estimator_)
